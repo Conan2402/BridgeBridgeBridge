ffac5f58ce0bcabb02cff49a05998d3ce670780e
Leandro Neumann <l.neumann@wnm.de>
2026-05-18 20:15:03 +0200

    Fix SkyBlock event notifications

--- src/minecraft/handlers/StateHandler.js
+++ src/minecraft/handlers/StateHandler.js
@@ -1,4 +1,5 @@
 const eventHandler = require("../../contracts/EventHandler.js");
+const { startEventNotifier, stopEventNotifier } = require("../other/eventNotifier.js");
 
 class StateHandler extends eventHandler {
   /** @param {import("minecraft-protocol").Client} minecraft */
@@ -9,14 +10,27 @@ class StateHandler extends eventHandler {
     this.loginAttempts = 0;
     this.exactDelay = 0;
     this.reconnectTimeout = null;
+    this.eventNotifierStarted = false;
   }
 
   registerEvents(bot) {
     this.bot = bot;
 
     this.bot.on("login", (...args) => this.onLogin(...args));
+    this.bot.on("spawn", (...args) => this.onSpawn(...args));
     this.bot.on("end", (...args) => this.onEnd(...args));
     this.bot.on("kicked", (...args) => this.onKicked(...args));
+
+    this.startNotifierOnce();
+  }
+
+  startNotifierOnce() {
+    if (this.eventNotifierStarted) {
+      return;
+    }
+
+    this.eventNotifierStarted = true;
+    startEventNotifier(this.bot);
   }
 
   onLogin() {
@@ -29,9 +43,18 @@ class StateHandler extends eventHandler {
       clearTimeout(this.reconnectTimeout);
       this.reconnectTimeout = null;
     }
+
+    this.startNotifierOnce();
+  }
+
+  onSpawn() {
+    this.startNotifierOnce();
   }
 
   onEnd(reason) {
+    stopEventNotifier();
+    this.eventNotifierStarted = false;
+
     if (reason && reason === "restart") {
       return;
     }
--- src/minecraft/other/eventNotifier.js
+++ src/minecraft/other/eventNotifier.js
@@ -1,62 +1,193 @@
 const { getSkyblockCalendar } = require("../../../API/functions/getCalendar.js");
 const minecraftCommand = require("../../contracts/minecraftCommand.js");
-const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 const config = require("../../../config.json");
 const axios = require("axios");
 
-if (config.minecraft.skyblockEventsNotifications.enabled) {
-  const { notifiers, customTime } = config.minecraft.skyblockEventsNotifications;
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
-  setInterval(async () => {
-    try {
-      const eventBOT = new minecraftCommand(bot);
-      eventBOT.officer = false;
-      const EVENTS = getSkyblockCalendar();
+let interval = null;
+let sentCache = new Set();
 
-      for (const event in EVENTS.events) {
-        const eventData = EVENTS.events[event];
-        if (notifiers[event] === false) {
-          continue;
-        }
+function startEventNotifier(bot) {
+  const notificationConfig = config.minecraft?.skyblockEventsNotifications;
 
-        if (eventData.events[0].start_timestamp < Date.now()) {
-          continue;
-        }
+  if (!notificationConfig?.enabled) {
+    return;
+  }
 
-        const minutes = Math.floor((eventData.events[0].start_timestamp - Date.now()) / 1000 / 60);
+  if (!bot) {
+    return;
+  }
 
-        let extraInfo = "";
-        if (event == "JACOBS_CONTEST") {
-          const { data: jacobResponse } = await axios.get("https://dawjaw.net/jacobs");
-          const jacobCrops = jacobResponse.find((crop) => crop.time >= Math.floor(eventData.events[0].start_timestamp / 1000));
+  if (interval) {
+    return;
+  }
 
-          if (jacobCrops?.crops !== undefined) {
-            extraInfo = ` (${jacobCrops.crops.join(", ")})`;
-          }
-        }
+  runEventCheck(bot);
 
-        const cTime = getCustomTime(customTime, event);
-        if (cTime.length !== 0 && cTime.includes(minutes.toString())) {
-          eventBOT.send(`[EVENT] ${eventData.name}${extraInfo}: Starting in ${minutes}m!`);
-          await delay(1500);
-        }
-
-        if (minutes == 0) {
-          eventBOT.send(`[EVENT] ${eventData.name}${extraInfo}: Starting now!`);
-          await delay(1500);
-        }
-      }
-    } catch (e) {
-      console.error(e);
-      /* empty */
-    }
+  interval = setInterval(() => {
+    runEventCheck(bot);
   }, 60000);
 }
 
-function getCustomTime(events, value) {
-  if (events === undefined || value === undefined) {
-    return false;
+function stopEventNotifier() {
+  if (!interval) {
+    return;
   }
 
-  return Object.keys(events).filter((key) => events[key].includes(value));
+  clearInterval(interval);
+  interval = null;
+  sentCache.clear();
 }
+
+async function runEventCheck(bot) {
+  try {
+    if (!bot) {
+      return;
+    }
+
+    const eventBOT = new minecraftCommand(bot);
+    eventBOT.officer = false;
+
+    const notificationConfig = config.minecraft?.skyblockEventsNotifications;
+    const notifiers = notificationConfig?.notifiers || {};
+    const customTime = notificationConfig?.customTime || {};
+
+    const calendar = await getSkyblockCalendar();
+
+    if (!calendar?.events) {
+      return;
+    }
+
+    const eventKeys = Object.keys(calendar.events);
+
+    for (const eventKey of eventKeys) {
+      const eventData = calendar.events[eventKey];
+
+      if (notifiers[eventKey] === false) {
+        continue;
+      }
+
+      const nextEvent = eventData?.events?.[0];
+
+      if (!nextEvent?.start_timestamp) {
+        continue;
+      }
+
+      const startTimestamp = nextEvent.start_timestamp;
+      const diffMs = startTimestamp - Date.now();
+
+      if (diffMs < 0) {
+        continue;
+      }
+
+      const minutes = Math.floor(diffMs / 1000 / 60);
+      const configuredTimes = getCustomTimes(customTime, eventKey);
+
+      let extraInfo = "";
+
+      if (eventKey === "JACOBS_CONTEST") {
+        extraInfo = await getJacobExtraInfo(startTimestamp);
+      }
+
+      const eventName = eventData.name || eventKey;
+
+      if (configuredTimes.includes(minutes.toString())) {
+        const cacheKey = `${eventKey}-${startTimestamp}-${minutes}`;
+
+        if (!sentCache.has(cacheKey)) {
+          sentCache.add(cacheKey);
+
+          sendEventMessage(
+            eventBOT,
+            `[EVENT] ${eventName}${extraInfo}: Starting in ${minutes}m!`
+          );
+
+          await delay(1500);
+        }
+      }
+
+      if (diffMs <= 60000 && diffMs > 0) {
+        const cacheKey = `${eventKey}-${startTimestamp}-now`;
+
+        if (!sentCache.has(cacheKey)) {
+          sentCache.add(cacheKey);
+
+          sendEventMessage(
+            eventBOT,
+            `[EVENT] ${eventName}${extraInfo}: Starting now!`
+          );
+
+          await delay(1500);
+        }
+      }
+    }
+
+    cleanSentCache();
+  } catch {
+    //
+  }
+}
+
+function getCustomTimes(customTime, eventKey) {
+  if (!customTime || !eventKey) {
+    return [];
+  }
+
+  return Object.keys(customTime).filter((minutes) => {
+    const events = customTime[minutes];
+
+    if (!Array.isArray(events)) {
+      return false;
+    }
+
+    return events.includes(eventKey);
+  });
+}
+
+async function getJacobExtraInfo(startTimestamp) {
+  try {
+    const { data } = await axios.get("https://dawjaw.net/jacobs");
+
+    if (!Array.isArray(data)) {
+      return "";
+    }
+
+    const jacobCrops = data.find(
+      (crop) => crop.time >= Math.floor(startTimestamp / 1000)
+    );
+
+    if (jacobCrops?.crops !== undefined) {
+      return ` (${jacobCrops.crops.join(", ")})`;
+    }
+
+    return "";
+  } catch {
+    return "";
+  }
+}
+
+function sendEventMessage(eventBOT, message) {
+  try {
+    if (!eventBOT || typeof eventBOT.send !== "function") {
+      return;
+    }
+
+    eventBOT.send(message);
+  } catch {
+    //
+  }
+}
+
+function cleanSentCache() {
+  if (sentCache.size < 500) {
+    return;
+  }
+
+  sentCache = new Set();
+}
+
+module.exports = {
+  startEventNotifier,
+  stopEventNotifier
+};
